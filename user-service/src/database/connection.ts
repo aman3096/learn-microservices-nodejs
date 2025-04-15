7607bc4e5980f26559857fda9f04965f9f499ded
Aman Tandon <aman.tandon3096@gmail.com>
2025-04-15 22:48:39 +0530

    feat: code init for microservices code

---
+++ user-service/src/database/connection.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import config from '../config/config';
+
+
+export const connectDB = async () => {
+    try {
+
+        console.log('Connecting to database...')
+        await mongoose.connect(config.MONGO_URI!);
+        console.info('Database connected');
+    } catch(error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
